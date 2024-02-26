c0dae560d46fdb5b864bc0981c3d93420f64b44a
Ashley Boehs <ashley@boehs.com>
2024-02-26 12:25:21 -0600

    Product and Order models/Checkout Form

--- Gemfile
+++ Gemfile
@@ -86,3 +86,7 @@ gem "sidekiq", "~> 6.2"
 gem "sitemap_generator", "~> 6.1"
 gem "whenever", require: false
 gem "responders", github: "heartcombo/responders", branch: "main"
+
+
+gem "stripe", "~> 10.9"
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    { success: "alert-sucess",
+      error: "alert-danger",
+      alert: "alert-warning",
+      notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
+  end
+
+  def formatted_amount(amount)
+    number_to_currency amount / 100.0
+  end
 end
